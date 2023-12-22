0b41ea3cf128bfc4027cde9e13fb443b73fa4063
Zara-Jacobson <zara.jacobson@post.runi.ac.il>
2023-12-22 12:54:31 +0200

    AddTwo Completed

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,23 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
-	}
-}
+		if (args.length == 2) {
+					try {
+						
+						int num1 = Integer.parseInt(args[0]);
+						int num2 = Integer.parseInt(args[1]);
+		
+						
+						int sum = num1 + num2;
+		
+						
+						System.out.println(num1 + " + " + num2 + " = " + sum);
+					} catch (NumberFormatException e) {
+						System.out.println("Error: Please provide valid integers as command-line arguments.");
+					}
+				} else {
+					System.out.println("Error: Please provide exactly two integers as command-line arguments.");
+				}
+			}
+		}
+	
